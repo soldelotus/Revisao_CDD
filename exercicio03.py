3094c2cfa8523aef687bab75da55da2818ea687b
soldelotus <sdelotus@gmail.com>
2025-04-22 15:50:54 -0300

    Exercício 03

---
+++ exercicio03.py
@@ -0,0 +1,6 @@
+salmin = float(input("Digite o salário mínimo atual: "))
+salpessoa = float(input("Digite o seu salário: "))
+
+divisao = salpessoa/salmin
+
+print(f"Você ganha {divisao:.1f} salário(s) mínimo(s)")
